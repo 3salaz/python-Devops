81742c5bcdd7327cbbff27a879996b53a0719e75
3salaz <3salaz.dev@gmail.com>
2019-05-11 11:02:21 -0700

    completed script two

--- scriptTwo.py
+++ scriptTwo.py
@@ -3,9 +3,8 @@
 def letterCount(word,letter):
     # Loop through word if i = letter wordCount + 1
     counter = 0
-    for x in 'word':
-        print(x)
+    for x in word:
         if x == letter :
-            counter += 1
-    return counter
-letterCount("missisipi", 'i')
+            counter = counter+1
+    return print(counter)
+letterCount("missisipi", "m")
